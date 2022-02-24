be056e327ad841597b1959a3451a8b068efd2c90
Teun Schaeken <teun.schaeken@gmail.com>
2022-02-24 15:58:57 +0100

    nothing to see here

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -73,7 +73,7 @@ const DEFINITIONS: Definition[] = [
     word: 'pando',
     definition: 'sinónimo de playo, bajo en profunidad. \nDe uso popular en Mendoza.',
     example:
-      "Maaa, Sofi no me presta el flota flota!!" "Basta eh, que los hago venir a lo pandito de la pile"
+      'Maaa, Sofi no me presta el flota flota!!" "Basta eh, que los hago venir a lo pandito de la pile'
   }
 ]
 
