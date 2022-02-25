49a6c86399557a9f69d54df7a1ca8c2e9d1ee641
Teun Schaeken <teun.schaeken@gmail.com>
2022-02-25 19:33:39 +0100

    keyboard to ascii in hope to fix motorola browser bug

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -52,7 +52,7 @@ export const Keyboard = ({
   return (
     <div>
       <div className="flex justify-center mb-1">
-        {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map((key) => (
+        {['\u0051', '\u0057', '\u0045', '\u0052', '\u0054', '\u0059', '\u0055', '\u0049', '\u004F', '\u0050'].map((key) => (
           <Key
             value={key}
             key={key}
@@ -63,7 +63,7 @@ export const Keyboard = ({
         ))}
       </div>
       <div className="flex justify-center mb-1">
-        {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ'].map((key) => (
+        {['\u0041', '\u0053', '\u0044', '\u0046', '\u0047', '\u0048', '\u004A', '\u004B', '\u004C', '\u00D1'].map((key) => (
           <Key
             value={key}
             key={key}
@@ -77,7 +77,7 @@ export const Keyboard = ({
         <Key width={65.4} value="ENTER" onClick={onClick}>
           {ENTER_TEXT}
         </Key>
-        {['Z', 'X', 'C', 'V', 'B', 'N', 'M'].map((key) => (
+        {['\u005A', '\u0058', '\u0043', '\u0056', '\u0042', '\u004E', '\u004D'].map((key) => (
           <Key
             value={key}
             key={key}
