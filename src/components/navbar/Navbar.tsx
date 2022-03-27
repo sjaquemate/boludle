2bdec3d23e2b871406c08517a61f7b5b1c4859ed
Teun Schaeken <teun.schaeken@gmail.com>
2022-03-27 12:10:29 +0200

    add donation banner

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,6 +11,15 @@ type Props = {
 export const Navbar = ({ setIsInfoModalOpen, setIsStatsModalOpen }: Props) => {
   return (
     <div className="navbar">
+
+      <div className="navbar-banner px-5 bg-slate-200">
+        <p className="text-sm italic mx-5 text-center">
+          <a className="underline font-bold" href="https://www.leer.org/donar">
+          apoyá
+          </a> {' '}
+          la lectura de los boludles más pequeños
+        </p>
+      </div>
       <div className="navbar-content px-5">
         <InformationCircleIcon
           className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
